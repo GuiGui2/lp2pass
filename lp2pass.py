c2dda821daadad61395c8df3715103bdfd5819fc
Guillaume Lasmayous <github@guigui2.net>
2015-02-21 18:45:55 +0100

    Handle string formatting properly

--- lp2pass.py
+++ lp2pass.py
@@ -6,20 +6,20 @@ from subprocess import Popen, PIPE
 
 def path_for(element, path=None):
 	""" Generate path name from elements name and current path """
-	path = "%s/%s" % (element[5], element[4]) 
+	path = "{0}/{1}".format(element[5], element[4]) 
 	return path
 
 def password_data(element):
-	URL = "URL: " + element[0]
-	ID = "ID: "+ element[1]
-	pw = "Password: " + element[2]
-	data = "%s\n%s\n%s" % (URL,ID, pw)
+	URL = "URL: {0}".format(element[0])
+	ID = "ID: {0}".format(element[1])
+	pw = "Password: {0}".format(element[2])
+	data = "{0}\n{1}\n{2}".format(URL,ID, pw)
 	return data
 
 def import_entry(element, path=''):
     """ Import new password entry to password-store using pass insert
     command """
-    print "Importing " + path_for(element, path)
+    print "Importing {0}".format(path_for(element, path))
     proc = Popen(['pass', 'insert', '--multiline', '--force',
                   path_for(element, path)],
               stdin=PIPE, stdout=PIPE)
