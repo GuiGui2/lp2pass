3988d14da064b12a290031af55bb6603cd1790cf
Guillaume Lasmayous <github@guigui2.net>
2015-02-22 12:31:34 +0100

    - Replace csv.reader by csv.DictReader, to be able to address to different elements by their names rather than with IDs. - Tab police to properly align everything

--- lp2pass.py
+++ lp2pass.py
@@ -7,34 +7,39 @@ from subprocess import Popen, PIPE
 
 def path_for(element, path=None):
 	""" Generate path name from elements name and current path """
-	path = "{0}/{1}".format(element[5], element[4]) 
+	path = "{0}/{1}".format(element['grouping'], element['name']) 
 	return path
 
 def password_data(element):
 	""" Extract data to fill each pass entry """
-	URL = "URL: {0}".format(element[0])
-	ID = "ID: {0}".format(element[1])
-	pw = "Password: {0}".format(element[2])
+	URL = "URL: {0}".format(element['url'])
+	ID = "ID: {0}".format(element['username'])
+	pw = "Password: {0}".format(element['password'])
 	data = "{0}\n{1}\n{2}".format(URL,ID, pw)
 	return data
 
 def import_entry(element, path=''):
-    """ Import new password entry to password-store using pass insert
-    command """
-    print "Importing {0}".format(path_for(element, path))
-    proc = Popen(['pass', 'insert', '--multiline', '--force',
-                  path_for(element, path)],
-              stdin=PIPE, stdout=PIPE)
-    proc.communicate(password_data(element).encode('utf8'))
-    proc.wait()
+    	""" Import new password entry to password-store using pass insert
+    	command """
+    	print "Importing {0}".format(path_for(element, path))
+    	proc = Popen(['pass', 'insert', '--multiline', '--force',
+                	path_for(element, path)],
+              		stdin=PIPE, stdout=PIPE)
+    	proc.communicate(password_data(element).encode('utf8'))
+    	proc.wait()
 
 def main(inputfile):
 	file = open(inputfile, "rb")
 	try:
-		reader = csv.reader(file)
+		reader = csv.DictReader(file)
+		imported = 0
 		for row in reader:
 			import_entry(row)
+			imported = imported + 1
 	finally:
+		print "======================================"
+		print "Imported {0} passwords into pass".format(imported)
+		print "======================================"
 		file.close()
 
 if __name__ == "__main__":
